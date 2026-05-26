9aa3208d3bd374fb0b64c6bc820338ecc2094e51
Olesya Saifullina <olraif@users.noreply.github.com>
2026-05-26 16:39:04 +0300

    Bump web app cache for Day AI

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tutor-app-v42";
+const CACHE = "tutor-app-v43";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json", "./icon-32.png", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
